b843fd4b847d09712890d843fe6fee55c1219f83
Aarav Elec <aarav010211@gmail.com>
2021-10-26 16:58:10 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,34 +2,46 @@ var SpeechRecognition = window.webkitSpeechRecognition;
   
 var recognition = new SpeechRecognition();
 
+var Textbox = document.getElementById("textbox"); 
+
 function start()
 {
-    document.getElementById("textbox").innerHTML = ""; 
+    Textbox.innerHTML = ""; 
     recognition.start();
 } 
  
 recognition.onresult = function(event) {
 
-     console.log(event); 
+ console.log(event); 
 
-    var Content = event.results[0][0].transcript;
+var Content = event.results[0][0].transcript;
 
-    document.getElementById("textbox").innerHTML = Content;
+    Textbox.innerHTML = Content;
     console.log(Content);
+      if(Content =="take my selfie")
+      {
+        console.log("taking selfie --- ");
         speak();
+      }
 }
 
 
 function speak(){
     var synth = window.speechSynthesis;
 
-    speak_data = document.getElementById("textbox").innerHTML;
+    speak_data = "Taking you Selfie in 5 seconds";
 
     var utterThis = new SpeechSynthesisUtterance(speak_data);
 
     synth.speak(utterThis);
 
     Webcam.attach(camera);
+
+    setTimeout(function()
+    { 
+        take_selfie(); 
+        save();
+    }, 5000);
 }
 
  
@@ -38,5 +50,21 @@ Webcam.set({
     width:360,
     height:250,
     image_format : 'jpeg',
-    jpeg_quality:100
+    jpeg_quality:90
 });
+
+function take_selfie()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+    });
+}
+
+
+function save()
+{
+  link = document.getElementById("link");
+  image = document.getElementById("selfie_image").src ;
+  link.href = image;
+  link.click();
+}
